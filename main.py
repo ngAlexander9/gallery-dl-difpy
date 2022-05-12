1abb3e28b63106752ffb0cc544627e8a08a46838
Alexander Ng <43122649+ngAlexander9@users.noreply.github.com>
2022-05-12 11:02:59 -0700

    action options

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import json
 import subprocess
 from difPy import dif
+import argparse
 
 def getconfig():
   try:
@@ -22,19 +23,36 @@ def getdirs(gallery_path, dirs):
       getdirs(dir_path, dirs)
 
 def main():
+  # Set up parser
+  parser = argparse.ArgumentParser()
+  single_action_group = parser.add_mutually_exclusive_group()
+  single_action_group.add_argument("-d", "--download-only", help = "only download (gallery-dl)", action="store_true")
+  single_action_group.add_argument("-c", "--cull-only", help="only cull (difpy)", action="store_true")
+  args = parser.parse_args()
+
   configdata = getconfig()
   parent_path = configdata['gallery-dl-parent-path']
   gallery_path = parent_path + '/gallery-dl'
   archive_file = configdata['archivefile']
   url_file = configdata['urlfile']
-
-  cmd = ['gallery-dl', '-T', '5', '-i', url_file, '--download-archive', archive_file]
-  subprocess.run(cmd, cwd=parent_path, shell=True)
   
-  dirs = []
-  getdirs(gallery_path, dirs)
-  for dir in dirs:
-    search = dif(dir, delete=True, silent_del=True)
+  if not args.cull_only:
+    # gallery-dl to download from urls
+    print("Downloading from list")
+    pass
+    cmd = ['gallery-dl', '-T', '5', '-i', url_file, '--download-archive', archive_file]
+    subprocess.run(cmd, cwd=parent_path, shell=True)
+
+  if not args.download_only:
+    # difpy to delete duplicate images
+    print("Deleting duplicate files")
+    pass
+    dirs = []
+    getdirs(gallery_path, dirs)
+    for dir in dirs:
+      search = dif(dir, delete=True, silent_del=True)
+  
+  
     
 
 if __name__ == "__main__":
